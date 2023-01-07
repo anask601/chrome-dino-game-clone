58ae037a72d79ef1c2bbd9b04d62c5bb8bf8d8e3
anask601 <anask60155@gmail.com>
2023-01-07 16:33:19 +0530

    Added update function to dynamiclly update the frame rate

--- script.js
+++ script.js
@@ -3,6 +3,19 @@ const WORLD_HEIGHT = 30;
 
 const worldElem = document.querySelector("[data-world]");
 
+let lastTime;
+const update = (time) => {
+  if (lastTime == null) {
+    lastTime = time;
+    window.requestAnimationFrame(update);
+    return;
+  }
+  const delta = time - lastTime;
+  lastTime = time;
+  window.requestAnimationFrame(update);
+};
+window.requestAnimationFrame(update);
+
 setPixelToWorldScale();
 window.addEventListener("resize", setPixelToWorldScale);
 function setPixelToWorldScale() {
