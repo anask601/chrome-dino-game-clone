31276333ef77d6209712222f154e3b2e21b8089b
anask601 <anask60155@gmail.com>
2023-01-14 20:57:56 +0530

    Updating score dynamicly and updating speed base on score

--- script.js
+++ script.js
@@ -2,10 +2,21 @@ import { updateGround, setupGround } from "./ground.js";
 
 const WORLD_WIDTH = 100;
 const WORLD_HEIGHT = 30;
+const SPEED_SCALE_INCREASE = 0.00001;
 
 const worldElem = document.querySelector("[data-world]");
+const scoreElem = document.querySelector("[data-score]");
+const startScreenElem = document.querySelector("[data-start-screen]");
+
+// setup pixel world
+setPixelToWorldScale();
+window.addEventListener("resize", setPixelToWorldScale);
+document.addEventListener("keydown", handleStart, { once: true });
 
 let lastTime;
+let speedScale;
+let score;
+
 const update = (time) => {
   if (lastTime == null) {
     lastTime = time;
@@ -13,15 +24,31 @@ const update = (time) => {
     return;
   }
   const delta = time - lastTime;
-  updateGround(delta);
+  updateGround(delta, speedScale);
+  updateSpeedScale(delta);
+  updateScore(delta);
   lastTime = time;
   window.requestAnimationFrame(update);
 };
-window.requestAnimationFrame(update);
 
-setPixelToWorldScale();
-window.addEventListener("resize", setPixelToWorldScale);
-setupGround();
+const updateSpeedScale = (delta) => {
+  speedScale += delta * SPEED_SCALE_INCREASE;
+};
+
+const updateScore = (delta) => {
+  score += delta * 0.01;
+  scoreElem.textContent = Math.floor(score);
+};
+
+function handleStart() {
+  lastTime = null;
+  speedScale = 1;
+  score = 0;
+  setupGround();
+  startScreenElem.classList.add("hide");
+  window.requestAnimationFrame(update);
+}
+
 function setPixelToWorldScale() {
   let worldToPixelScale;
   if (window.innerWidth / window.innerHeight < WORLD_WIDTH / WORLD_HEIGHT) {
